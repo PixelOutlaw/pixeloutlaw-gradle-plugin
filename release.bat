97aaba235d62f78e8b8d0f88f32fbff553fcb580
Richard Harrah <topplethenunnery@gmail.com>
2020-12-28 13:38:26 -0500

    add release.bat

---
+++ release.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+cd /d pixeloutlaw-gradle-plugin
+set releaseScope=patch
+set /p releaseScope=Release scope (default - %releaseScope%)?:
+gradlew.bat clean final publishPlugins -Prelease.scope=%releaseScope% -Pgit.root=%~dp0
