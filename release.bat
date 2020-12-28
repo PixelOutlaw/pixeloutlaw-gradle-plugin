6d0cae1e30c7d1020f68b65ff26f6dc182e97aa2
Richard Harrah <topplethenunnery@gmail.com>
2020-12-28 13:39:02 -0500

    run parent gradlew.bat in release.bat

--- release.bat
+++ release.bat
@@ -3,4 +3,4 @@
 cd /d pixeloutlaw-gradle-plugin
 set releaseScope=patch
 set /p releaseScope=Release scope (default - %releaseScope%)?:
-gradlew.bat clean final publishPlugins -Prelease.scope=%releaseScope% -Pgit.root=%~dp0
+..\gradlew.bat clean final publishPlugins -Prelease.scope=%releaseScope% -Pgit.root=%~dp0
