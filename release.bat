c7950810cf172c6726291ab8dddeff074b2d3aea
Richard Harrah <topplethenunnery@gmail.com>
2020-12-28 13:39:38 -0500

    don't change directory by running script

--- release.bat
+++ release.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 
+set parent=%cd%
 cd /d pixeloutlaw-gradle-plugin
 set releaseScope=patch
 set /p releaseScope=Release scope (default - %releaseScope%)?:
 ..\gradlew.bat clean final publishPlugins -Prelease.scope=%releaseScope% -Pgit.root=%~dp0
+cd /d %parent%
